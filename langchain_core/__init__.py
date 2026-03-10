f120bba63b509115462342157c7e45c82fc3316a
Augusto de Souza Cavalcanti <83598081+guto0604@users.noreply.github.com>
2026-03-10 15:52:16 -0300

    Improve LangSmith end_run error details

--- langchain_core/__init__.py
+++ langchain_core/__init__.py
@@ -1,3 +1,6 @@
 from .prompts import ChatPromptTemplate
 from .runnables import RunnableConfig, RunnableLambda, RunnableParallel, RunnablePassthrough, RunnableSequence
 from .output_parsers import StrOutputParser
+from . import version
+
+__version__ = version.__version__
